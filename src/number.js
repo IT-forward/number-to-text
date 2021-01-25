c6101a086636ff51e528a3ef643d94ddb549e775
Husan-Eshonqulov <husan.eshonqulov.00@gmail.com>
2021-01-25 23:05:13 +0300

    with mistakes

--- src/number.js
+++ src/number.js
@@ -1,6 +1,6 @@
 let ones = ['', 'bir', 'ikki', 'uch', 'to‘rt', 'besh', 'olti', 'yetti', 'sakkiz', 'to‘qqiz'];
 let tens = ['', 'o‘n', 'yigirma', 'o‘ttiz', 'qirq', 'ellik', 'oltmish', 'yetmish', 'sakson', 'to‘qson'];
-
+let nameOfGroup = ['', 'ming', 'million', 'milliard', 'triliard']; 
 
 function hundredsToText(num) {
     let res = '';
@@ -26,7 +26,7 @@ function onesToText(num){
     return ones[num % 10];
 }
 
-function convert(num) {
+function threeDigitsGroupToText(num) {
     let textForm = '';
 
     if(typeof num !== 'number'){
@@ -46,6 +46,59 @@ function convert(num) {
     } 
 
     return textForm.trim(); 
+}
+
+function numberOfDigits(num){
+    let res = 0;
+    
+    while (num > 0){
+      num = Math.floor(num / 10); // 
+      res++;
+    }
+
+    return res;
+}
+
+function groupingDigits(num){   
+    let groupsOfDigits = [];
+    let amountOfThreeDigitsGroups = Math.ceil(numberOfDigits(num) / 3); 
+    while (amountOfThreeDigitsGroups > 0){
+        let group = Math.floor(num / Math.pow(1000, amountOfThreeDigitsGroups - 1)); 
+        groupsOfDigits.push(group); 
+        num %= Math.pow(1000, amountOfThreeDigitsGroups - 1);  
+        amountOfThreeDigitsGroups--; 
+    }
+
+    return groupsOfDigits.reverse();
+}
+
+function convert(num){
+    let numberToText = '';
+    let groups = groupingDigits(num);
+    let length = groups.length;  
+  
+    function allIsZero(groups){
+        for (let i = 0; i < length - 1; i++){
+            if (groups[i] !== 0){
+                return false;
+            }
+        }
+      
+        return true;
+    }  
+  
+    if (groups[length - 1] === 1 && allIsZero(groups)){
+        numberToText += nameOfGroup[length - 1];
+    }
+    else {
+        for (let i = length - 1; i >= 0; i--){
+          if (groups[i] != 0){
+            numberToText += " " + threeDigitsGroupToText(groups[i]) + " " + nameOfGroup[i]; 
+          } 
+        }
+    }
+
+    return numberToText.trim();
 }
 
 export default convert;
